e481aad8a333d53080d6003840f37ba9dfa9d461
Jitendrapranto <jitendrapranto@gmail.com>
2025-05-19 14:59:17 +0600

    todo completed

---
+++ todo.js
@@ -0,0 +1,26 @@
+const todo = [
+  { title: "Do Home Work", done: true },
+  { title: "Learn js", done: false },
+  { title: "Make a simple project", done: true },
+  { title: "Understanding the array", done: false },
+];
+let completedTask = [];
+let ongoingTask = [];
+
+todo.forEach((todoitem) => {
+  if (todoitem.done) {
+    completedTask.push(todoitem);
+  } else {
+    ongoingTask.push(todoitem);
+  }
+});
+
+console.log("✅ Completed Tasks");
+completedTask.forEach((task) => {
+  console.log(`- ${task.title}`);
+});
+
+console.log("\n🕐 Ongoing Tasks");
+ongoingTask.forEach((task) => {
+  console.log(`- ${task.title}`);
+});
